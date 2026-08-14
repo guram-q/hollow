93f79f9874116f0e7daaa8775ba5396492f7d409
freegrapher <g.qveladze@gmail.com>
2026-08-14 22:46:55 +0400

    ticker

--- Build/web.loader.js
+++ Build/web.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 59869518,
+    wasmFileSize: 59869544,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
